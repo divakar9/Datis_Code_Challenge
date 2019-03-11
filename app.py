1d8f015bfb15720440f9459b6811d1c573dda0b2
divakar9 <aashumer@gmail.com>
2019-03-11 01:34:50 -0500

    final

--- app.py
+++ app.py
@@ -7,34 +7,73 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGODB_URI", "mongodb://localhost:27017/myDatabase")
 mongo = PyMongo(app)
 
-@app.route("/addEmployee", methods=['POST'])
-def addEmployee():
-    try:
-        json_data = request.json['info']
-        employeeName = json_data['employee']
-        jobtitle = json_data['job']
-        basesalary = json_data['salary']
-        medicalinsurance = json_data['insurance_pay']
-        take_home_pay = json_data['real_pay']
-        mongo.db.Employees.insert_one({
-            'employee': employeeName, 'job': jobtitle, 'salary': basesalary, 'insurance_pay': medicalinsurance, 'real_pay': take_home_pay
-        })
-        return jsonify(status='OK', message='inserted successfully')
-    except Exception as e:
-        return jsonify(status='ERROR', message=str(e))
-
 
 @app.route('/')
 def showEmployeelist():
     return render_template('list.html')
 
 
-@app.route('/getEmployee', methods=['POST'])
-def getEmployee():
-    try:
-        employeeId = request.json['id']
-        employee = mongo.db.Employees.find_one({'_id': ObjectId(employeeId)})
-        employeeDetail = {
+@app.route("/addEmployee", methods=['POST'])
+def addEmployee():
+    json_data = request.json['info']
+
+    # check type of incoming data
+    # data_check.check_types(json_data)
+
+    employeeName = json_data['employee']
+    jobtitle = json_data['job']
+    basesalary = float(json_data['salary'])
+    medicalinsurance = float(json_data['insurance_pay'])
+    take_home_pay = float(json_data['real_pay'])
+
+    mongo.db.Employees.insert_one({
+            'employee': employeeName, 'job': jobtitle, 'salary': basesalary, 'insurance_pay': medicalinsurance, 'real_pay': take_home_pay
+        })
+
+    return jsonify(status='OK', message='inserted successfully')
+
+
+@app.route('/getEmployee/<eid>', methods=['GET'])
+def getEmployee(eid):
+    employeeId = eid
+    employee = mongo.db.Employees.find_one({'_id': ObjectId(employeeId)})
+    employeeDetail = {
+        'employee': employee['employee'],
+        'job': employee['job'],
+        'salary': employee['salary'],
+        'insurance_pay': employee['insurance_pay'],
+        'real_pay': employee['real_pay'],
+        'id': str(employee['_id'])
+    }
+
+    return json.dumps(employeeDetail)
+
+
+
+@app.route('/updateEmployee', methods=['PUT'])
+def updateEmployee():
+    employeeInfo = request.json['info']
+    # data_check.check_types(employeeInfo)
+
+    employeeId = employeeInfo['id']
+    employeeName = employeeInfo['employee']
+    jobtitle = employeeInfo['job']
+    basesalary = float(employeeInfo['salary'])
+    medicalinsurance = float(employeeInfo['insurance_pay'])
+    take_home_pay = float(employeeInfo['real_pay'])
+
+    mongo.db.Employees.update_one({'_id': ObjectId(employeeId)}, {
+        '$set': {'employee': employeeName, 'job': jobtitle, 'salary': basesalary, 'insurance_pay': medicalinsurance, 'real_pay': take_home_pay}})
+    return jsonify(status='OK', message='updated successfully')
+
+
+@app.route("/getEmployeeList", methods=['GET'])
+def getEmployeeList():
+    employees = mongo.db.Employees.find()
+    employeeList = []
+
+    for employee in employees:
+        employeeItem = {
             'employee': employee['employee'],
             'job': employee['job'],
             'salary': employee['salary'],
@@ -42,49 +81,9 @@ def getEmployee():
             'real_pay': employee['real_pay'],
             'id': str(employee['_id'])
         }
-        return json.dumps(employeeDetail)
-    except Exception as e:
-        return str(e)
 
+        employeeList.append(employeeItem)
 
-
-@app.route('/updateEmployee', methods=['PUT'])
-def updateEmployee():
-    try:
-        employeeInfo = request.json['info']
-        employeeId = employeeInfo['id']
-        employeeName = employeeInfo['employee']
-        jobtitle = employeeInfo['job']
-        basesalary = employeeInfo['salary']
-        medicalinsurance = employeeInfo['insurance_pay']
-        take_home_pay = employeeInfo['real_pay']
-
-        mongo.db.Employees.update_one({'_id': ObjectId(employeeId)}, {
-            '$set': {'employee': employeeName, 'job': jobtitle, 'salary': basesalary, 'insurance_pay': medicalinsurance, 'real_pay': take_home_pay}})
-        return jsonify(status='OK', message='updated successfully')
-    except Exception as e:
-        return jsonify(status='ERROR', message=str(e))
-
-
-@app.route("/getEmployeeList", methods=['GET'])
-def getEmployeeList():
-    try:
-        employees = mongo.db.Employees.find()
-
-        employeeList = []
-        for employee in employees:
-            print(employee)
-            employeeItem = {
-                'employee': employee['employee'],
-                'job': employee['job'],
-                'salary': employee['salary'],
-                'insurance_pay': employee['insurance_pay'],
-                'real_pay': employee['real_pay'],
-                'id': str(employee['_id'])
-            }
-            employeeList.append(employeeItem)
-    except Exception as e:
-        return str(e)
     return json.dumps(employeeList)
 
 @app.route("/deleteEmployee", methods=['POST'])
